9946653ad31326567fbc162ef7ab40c92456851d
Ayush Shukla <ashukla@groupon.com>
2017-10-11 16:29:02 +0530

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RubyRailsUnicornRackSample::Application.config.secret_token = 'c247856eeb304d73e47f387b05ec5d8fccad629907da52a8e5a26ef5476d23bc58e48336cb8f10b340cc3d6c4b748e9ec81d909443afa94433679e9683862e32'
